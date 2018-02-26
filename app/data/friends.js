5c173f8a13c444648959a5a17ab76471fb3fc223
Elizabeth <erischjanson@gmail.com>
2018-02-26 13:35:17 -0600

    home.html skeleton and api routes started

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends= [
+	{
+		name: "Elizabeth",
+		photo: "placeholder.jpg",
+		scores: [
+			5,
+			2,
+			5,
+			4,
+			3,
+			1,
+			5,
+			3,
+			4,
+			4
+		]
+	}
+];
+
+module.exports = friends;

f977dd032d22f599c2458a9b95b441e97bc97ac6
Elizabeth <erischjanson@gmail.com>
2018-03-30 16:05:12 -0500

    cleaned up server.js

--- server.js
+++ server.js
@@ -9,19 +9,10 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-// app.use(function(req,res,next){
-// 	req.body.age--;
-// 	next();
-// });
 
 app.use("/api", apiRouter);
 app.use("/", htmlRouter);
-// app.post("/elizabeth", function(req, res){
-// 	console.log(req.body);
-	
-// 	// res.sendFile(__dirname + "/app/public/home.html");
-// 	res.status(202).json({message: "request body", data:req.body});
-// })
+
 
 require("./app/routing/apiRoutes.js");
 require("./app/routing/htmlRoutes.js");

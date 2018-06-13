c41bfbada5570d4c8b2a9cfe055067288e1ecc06
Elizabeth <erischjanson@gmail.com>
2018-06-13 13:38:23 -0500

    linked css file

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "app/public")));
+
+// Serve static content for the app from the "public" directory in the application directory.
+
 
 app.use("/api", apiRouter);
 app.use("/", htmlRouter);
+
+
 
 
 require("./app/routing/apiRoutes.js");
